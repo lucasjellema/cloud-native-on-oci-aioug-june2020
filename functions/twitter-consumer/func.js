b1659156054b8102aa61e28ae0b69107fdfd1b5a
Lucas Jellema <lucas.jellema@amis.nl>
2020-06-22 10:56:29 +0200

    func using query parameters

--- functions/twitter-consumer/func.js
+++ functions/twitter-consumer/func.js
@@ -1,16 +1,22 @@
-const fdk=require('@fnproject/fdk');
-const tweetConsumer=require('./index')
+const fdk = require('@fnproject/fdk');
+const tweetConsumer = require('./index')
 const url = require('url');
-fdk.handle(async function(input, ctx){
+fdk.handle(async function (input, ctx) {
   console.log(`TweetConsumer invoked with body ${JSON.stringify(input)}`)
   const requestURL = ctx.headers["Fn-Http-Request-Url"][0]
   console.log(`TweetConsumer invoked with Request URL ${JSON.stringify(requestURL)}`)
   //[\"/my-depl1/consume-tweets?hashtag=tulsa\u0026minutes=300\"]
   const queryData = url.parse(requestURL, true).query;
-  console.log(`queryDate = ${JSON.stringify(queryData)}`)
   
-  const x = await tweetConsumer.produceTweetReport( input.hashtag?input.hashtag:"AIOUG", input.minutes?input.minutes:5)
-  x.url= ctx.headers["Fn-Http-Request-Url"]
+  const hashtag = queryData ?
+    queryData.hashtag ? queryData.hashtag : input.hashtag ? input.hashtag : "AIOUG"
+    : input.hashtag ? input.hashtag : "AIOUG"
+
+  const minutes = queryData ?
+    queryData.minutes ? queryData.minutes : input.minutes ? input.minutes : 5
+    : input.minutes ? input.minutes : 5
+  const x = await tweetConsumer.produceTweetReport(hashtag, minutes)
+  x.url = ctx.headers["Fn-Http-Request-Url"]
   x.queryData = queryData
   return x
 })
