80f1b58900c08e508bc0709aae7d3e87b19723c2
Lucas Jellema <lucas.jellema@amis.nl>
2020-06-22 10:47:16 +0200

    func

--- functions/twitter-consumer/func.js
+++ functions/twitter-consumer/func.js
@@ -1,8 +1,11 @@
 const fdk=require('@fnproject/fdk');
 const tweetConsumer=require('./index')
+const url = require('url');
 fdk.handle(async function(input, ctx){
   console.log(`TweetConsumer invoked with body ${JSON.stringify(input)}`)
   console.log(`TweetConsumer invoked with Request URL ${JSON.stringify(ctx.headers["Fn-Http-Request-Url"])}`)
+  const queryData = url.parse(ctx.headers["Fn-Http-Request-Url"], true).query;
+  console.log(`queryDate = ${JSON.stringify(queryData)}`)
   
   const x = await tweetConsumer.produceTweetReport( input.hashtag?input.hashtag:"AIOUG", input.minutes?input.minutes:5)
   x.url= ctx.headers["Fn-Http-Request-Url"]
